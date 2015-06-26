55c314dbe931658353fdf743bfcb2c7a201ca3a7
magedavee <dvdavee@email.wm.edu>
2015-06-26 04:14:10 -0400

    add more functions to SecParticle.py

--- python/SecParticleEvt.py
+++ python/SecParticleEvt.py
@@ -1,5 +1,7 @@
 import ROOT 
 from ROOT import gROOT
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SecParticleEvt:
     def __init__(self,filename):
@@ -8,17 +10,44 @@ class SecParticleEvt:
 	 gROOT.ProcessLine(".L /home/mage/Local/include/SecParticleEvt.hh")
 	 self.sp=ROOT.SecParticleEvt(filename)
 
-###############sets the event number##########################
+###############sets the event number###########################
     def setEvtNum(self,num):
 	self.sp.SetNum(num)
 
-##Gets an event, return the current event if no num is given##
-    def getEvt(self,num):
+##Gets an event, return the current event if no num is given###
+    def getEvt(self,num=None):
 	if num is None:
 	    return self.sp.GetParticleEvent()
 	else:
 	    return self.sp.GetParticleEvent(num)
 
-################Gets a vertex in this event###################
+################Gets a vertex in this event####################
     def getVertex(self,num):
 	return self.sp.GetVertex(num)
+
+##############Get the total numeber of Event####################
+    def getTotEvt(self):
+	return self.sp.GetNumMax()
+
+##############Get the current Event number######################
+    def getEvtNum(self):
+	return self.sp.GetNum()
+
+##############Get the number of detectec for current event######
+    def getNumParticle(self):
+	return self.sp.GetParticleEvent().nParticlesDet
+    
+#############Shows hist of the number of particles detected######
+    def showParticlesDetHis(self):
+	total=self.getTotEvt()
+	num=[]
+	for i in xrange(0,total):
+	    det=self.getEvt(i).nParticlesDet
+	    num.append(det)
+	plt.hist(num)
+	plt.ylabel("frequency")
+	plt.title("Number of Particles detected")
+	plt.show()
+
+
+
